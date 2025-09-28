823e838ed79cfcde629933f8079a4c26b3f1e0cc
CruvixDev <100412344+CruvixDev@users.noreply.github.com>
2025-09-28 13:45:52 +0200

    Use ceedling for tests and remove Unity/CMock manual integration

--- tests/ceedling/test/test_test1.c
+++ tests/ceedling/test/test_test1.c
@@ -17,10 +17,3 @@ void test_sum_negative(void) {
     int result = -2 + -3;
     TEST_ASSERT_EQUAL_INT(-5, result);
 }
-
-int main(void) {
-    UNITY_BEGIN();
-    RUN_TEST(test_sum);
-    RUN_TEST(test_sum_negative);
-    return UNITY_END();
-}
